4fbad98e11e5b2411f7f461234913c39c7c517a4
Alexander Comerford <alex@taoa.io>
2025-02-16 06:58:05 +0000

    Add assert

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,19 @@
 pub mod tetris_board;
 
 pub use tetris_board::*;
+
+pub const ASSERT_LEVEL: u32 = 1;
+
+/// This custom assert is so we can can disable it at compile
+/// time, to remove runtime impact.
+#[macro_export]
+macro_rules! assert_level {
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        {
+            if $crate::ASSERT_LEVEL >= 1 {
+                assert!($($arg)*);
+            }
+        }
+    };
+}
