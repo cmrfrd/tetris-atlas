c0a391e2a557f1353dfa42fa7c948b8213f6468a
Alexander Comerford <alex@taoa.io>
2025-02-16 06:57:58 +0000

    add benchmark for tetris piece trait

---
+++ benches/tetris_piece.rs
@@ -0,0 +1,31 @@
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use tetris_atlas::{Rotation, TetrisPiece};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let pieces = black_box(
+        (0..10_000)
+            .map(|_| TetrisPiece(rand::random::<u8>() % 7))
+            .collect::<Vec<_>>(),
+    );
+    c.bench_function("num_rotations", |b| {
+        b.iter(|| pieces.iter().map(|p| p.num_rotations()).collect::<Vec<_>>())
+    });
+
+    let rotations = black_box(
+        (0..10_000)
+            .map(|_| Rotation(rand::random::<u8>() % 4))
+            .collect::<Vec<_>>(),
+    );
+    c.bench_function("dimensions", |b| {
+        b.iter(|| {
+            pieces
+                .iter()
+                .zip(rotations.iter())
+                .map(|(p, r)| p.width(*r))
+                .collect::<Vec<_>>()
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
